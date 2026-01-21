7cc465ede101e8f46488d54fd11674dfbb301bdb
Hygor Ostrowskij de Morais <hygor.o.morais@gmail.com>
2026-01-21 16:08:40 -0300

    Remove unnecessary/unused/draft code

--- src/constants.hpp
+++ src/constants.hpp
@@ -10,12 +10,6 @@ constexpr auto MY_SERVICE_DISPLAY_NAME = L"Interception Driver Fix";
 constexpr auto MY_SERVICE_DESCRIPTION  = L"Fixes reenumeration issues for the Interception Driver.";
 constexpr auto MY_DATA_DIR_NAME        = "Interception Driver Fix";
 constexpr auto MY_CFG_INI_NAME         = "interception-driver-fix.ini";
-// constexpr auto MY_UNINSTALL_REGISTRY_KEY_NAME = L"_Witsy_Test";
-// constexpr auto MY_INSTALL_PATH = "C:/ProgramData/Witsy/InterceptionDriverFix/interception_driver_fix.exe";
-
-
-// constexpr auto MY_INSTALL_UNINSTALL_MUTEX_STABLE_KEY_NAME   = L"SOFTWARE";
-// constexpr auto MY_INSTALL_UNINSTALL_MUTEX_VOLATILE_KEY_NAME = L"Witsy.tmp\\InstallUninstallMutex";
 
 
 }  // namespace
